207c7883663d1ba74bbca04732985c8e0220c355
Vinecastro <141651589+Vinecastro@users.noreply.github.com>
2024-05-20 11:35:27 -0300

    Add files via upload

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { FaUser, FaLock } from "react-icons/fa";
+import "./Login.css";
+
+const Login = () => {
+  // Estados para armazenar as entradas do usuário
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Função que é chamada quando o formulário é enviado
+  const handleSubmit = (event) => {
+    // Impede que a página seja recarregada
+    event.preventDefault();
+
+    // Faz o console log das credenciais do usuário
+    console.log("Dados de Login:", { username, password });
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <h1>Acesse a agenda</h1>
+        <div className="input-field">
+          <input
+            type="text"
+            placeholder="E-mail"
+            required
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <FaUser className="icon" />
+        </div>
+        <div className="input-field">
+          <input
+            type="password"
+            placeholder="Senha"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FaLock className="icon" />
+        </div>
+        <button type="submit">Login</button>
+        <div className="signup-link">
+          <p>
+            Não tem uma conta? <a href="#">Registar</a>{" "}
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
